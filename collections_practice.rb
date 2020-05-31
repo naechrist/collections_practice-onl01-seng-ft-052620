e8485bb0d2a2abe108b769753f1d178bfaed0371
Jenae Christensen <729jenae.christensen@gmail.com>
2020-05-31 18:24:52 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -32,7 +32,7 @@ array = ["blake", "ashley", "scott"]
 def kesha_maker(array)
   new_array = []
    new_array.each do |i| 
-        i[2] = '$' << array
+        i[2] = '$' >> array
     end
   
 end
